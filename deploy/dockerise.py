9d1912d372aacc44299fb69062b94cce95c8bbf1
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-02-12 12:43:45 +0530

    docker file checker function added

--- deploy/dockerise.py
+++ deploy/dockerise.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 import os
 
 
-def createdockerfile(project:str):
+def createdockerfile(project):
     curr_dir = os.getcwd()
     root_dir = os.path.join(curr_dir, project)
-	# print(curr_dir)
+
+    # print(curr_dir)
+
     f = os.path.join(root_dir, 'Dockerfile')
     readfile = open('./deploy/template_files/' + 'docker' + '.txt', 'r')
     with open(f, 'w+') as dockerfile:
         for line in readfile:
             dockerfile.write(line)
 
+
+def dockerfilechecker(project):
+    curr_dir = os.getcwd()
+    root_dir = os.path.join(curr_dir, project)
+    rootfiles = os.listdir(root_dir)
+    dockerfilename = 'Dockerfile'
+    if dockerfilename in rootfiles:
+        return True
+    return False

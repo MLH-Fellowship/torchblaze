b7b562f3edb0fe68835697dcd44ac6d5f64176a2
Aman Sharma <amansharma2910@gmail.com>
2021-02-12 12:10:12 +0530

    Update dockerise.py

--- deploy/dockerise.py
+++ deploy/dockerise.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 import os
-
 
 def createdockerfile(project:str):
     curr_dir = os.getcwd()
     root_dir = os.path.join(curr_dir, project)
 	# print(curr_dir)
     f = os.path.join(root_dir, 'Dockerfile')
-    readfile = open('./deploy/template_files/' + 'docker' + '.txt', 'r')
+    readfile = open('./deploy/template_files/' + 'docker.txt', 'r')
     with open(f, 'w+') as dockerfile:
         for line in readfile:
             dockerfile.write(line)

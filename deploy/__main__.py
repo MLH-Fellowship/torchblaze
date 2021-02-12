07c1410ee43679371f9c935e2aaa9a75788a27f7
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-02-12 12:05:52 +0530

    calling the create dockerfile in __main__.py

--- deploy/__main__.py
+++ deploy/__main__.py
@@ -1,5 +1,6 @@
 import fire
 from template import startproject
+from dockerise import createdockerfile
 
 class Deploy(object):
 
@@ -9,8 +10,9 @@ class Deploy(object):
   def generate_template(self, project_name):
     startproject(project_name)
 
-  def generate_docker(self, x, y):
-    pass
+  def generate_docker(self,project_name):
+  	createdockerfile(project_name)
+    
 
 if __name__ == '__main__':
   fire.Fire(Deploy)

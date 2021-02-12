98539a4af903083bfb394b34320253c5ca76e593
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-02-12 12:44:12 +0530

    docker file checker conditions added to createdocker function

--- deploy/__main__.py
+++ deploy/__main__.py
@@ -1,18 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import fire
 from template import startproject
-from dockerise import createdockerfile
+from dockerise import createdockerfile, dockerfilechecker
+import os
+
 
 class Deploy(object):
 
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
 
-  def generate_template(self, project_name):
-    startproject(project_name)
+    def generate_template(self, project_name):
+        startproject(project_name)
 
-  def generate_docker(self,project_name):
-  	createdockerfile(project_name)
-    
+    def generate_docker(self, project_name):
+        if not dockerfilechecker(project_name):
+        	createdockerfile(project_name)
+        	print('Default Docker File Created')
+       	else:
+            print('Dockerfile already present')
+
 
 if __name__ == '__main__':
-  fire.Fire(Deploy)
+    fire.Fire(Deploy)

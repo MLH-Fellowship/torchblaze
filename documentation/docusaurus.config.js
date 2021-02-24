1ef54238d489afffdf85f724b69b7d199526ccdc
Aman Sharma <amansharma2910@gmail.com>
2021-02-24 19:12:56 +0530

    Update docusaurus.config.js

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -18,7 +18,7 @@ module.exports = {
       items: [
         {
           to: 'docs/installation',
-          activeBasePath: 'installation',
+          activeBasePath: 'docs',
           label: 'Docs',
           position: 'left',
         },

1e5bcef1a26a90ff5e7ea92d4f5061720a6b132f
Aman Sharma <amansharma2910@gmail.com>
2021-02-26 20:55:49 +0530

    Add comments to the template.py file

--- torchblaze/template.py
+++ torchblaze/template.py
@@ -17,35 +17,51 @@ def startproject(project: str):
         # creating the main project directory
         os.mkdir(root_dir) 
 
-        # creating the files in root project directory
-        f = os.path.join(root_dir, ".gitignore")
-        with open(f, "w+") as gitignore:
-            gitignore.write(" ")
         
+        # creating the files in root project directory
+        f = os.path.join(root_dir, ".gitignore")  # path to the gitignore file to be created
+        with open(f, "w+") as gitignore:  # creating the .gitignore file 
+            gitignore.write(" ")  # writing to the gitignore file
+        
+        
+        # same creation logic as the .gitignore file
         f = os.path.join(root_dir, "README.md")
         with open(f, "w+") as writefile:
             writefile.writelines(pkg_resources.resource_string('torchblaze', 'template_files/README.txt').decode('utf-8').split('\n'))
 
+            
+        # same creation logic as the .gitignore file
         f = os.path.join(root_dir, "requirements.txt")
         with open(f, "w+") as writefile:
             writefile.writelines(pkg_resources.resource_string('torchblaze', 'template_files/requirements.txt').decode('utf-8').split('\n'))
         
+        
+        # same creation logic as the .gitignore file
         f = os.path.join(root_dir, "app.py")
         with open(f, "w+") as writefile:
             writefile.writelines(pkg_resources.resource_string('torchblaze', 'template_files/app.py').decode('utf-8').split('\n'))
         
+        
+        # same creation logic as the .gitignore file
         f = os.path.join(root_dir, "tests.json")
         with open(f, "w+") as writefile:
             writefile.writelines(pkg_resources.resource_string('torchblaze', 'template_files/tests.txt').decode('utf-8').split('\n'))
 
+            
+        # same creation logic as the .gitignore file
         f = os.path.join(root_dir, "Procfile")
         with open(f, "w+") as writefile:
             writefile.writelines(pkg_resources.resource_string('torchblaze', 'template_files/procfile.txt').decode('utf-8').split('\n'))
         
-        # creating the model directory and sub-dir/files
+        # creating the model directory
         model_dir = os.path.join(root_dir, 'model')
         os.mkdir(model_dir)
+        
+        # crating data sub-directory in the model directory
         os.mkdir(os.path.join(model_dir, 'data'))
+        
+        
+        # creating template files in model directory
         model_files = ['utils', 'model', 'train']
         
         for file in model_files:
